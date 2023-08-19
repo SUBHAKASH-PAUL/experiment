e55df68730d03a60d9ec1906231fa5862aa47f87
Subhakash Paul <subhopaul2010@gmail.com>
2023-08-19 11:26:59 +0530

    experiment

---
+++ gitc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("Enter a 1st Value: ");
+    scanf("%d", &a);
+    printf("Enter a 2nd VAlue: ");
+    scanf("%d", &b);
+    int sum=0;
+    sum = a+b;
+    printf("%d", sum);
+}
